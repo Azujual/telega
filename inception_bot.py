eb3bf2e1eaa29204a0ce3287bd82b3611147b200
HQTOP\iam <serghei@infineon.md>
2017-05-25 00:05:33 +0300

    qweqw

--- inception_bot.py
+++ inception_bot.py
@@ -17,14 +17,16 @@ def srv_status(hostname):
         s.login (hostname, username, port=1322)
         s.sendline ('uptime')   # run a command
         s.prompt()             # match the prompt
-        print s.before          # print everything before the prompt.
-        s.sendline ('ps auxww | grep -i 3proxy | grep -v grep | grep -v srati |wc -l')
-        s.prompt()
-        print s.before
+
+        #print s.before          # print everything before the prompt.
+        #s.sendline ('ps auxww | grep -i 3proxy | grep -v grep | grep -v srati |wc -l')
+        #s.prompt()
+        #print s.before
         s.logout()
     except pxssh.ExceptionPxssh, e:
         print "pxssh failed on login."
         print str(e)
+    return s.prompt
 
 def getMicb():
     selector = '<div id="currancy-rates">'
