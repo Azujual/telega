4de1da3a589f0d45559614c6fae8707b274b3a7b
HQTOP\iam <serghei@infineon.md>
2017-05-29 22:43:31 +0300

    Remove all comments which are not used.

--- inception_bot.py
+++ inception_bot.py
@@ -9,8 +9,6 @@ from pexpect import pxssh
 import getpass
 import sys
 
-#BASE Branch
-
 def input_check_ip(innput):
     pr = []
     file = open('list.lst', 'r')
@@ -42,27 +40,6 @@ def srv_status(hostname):
         return b
     except pxssh.ExceptionPxssh, e:
         return "pxssh failed on login."
-        #return str(e)
-
-def getMicb():
-    selector = '<div id="currancy-rates">'
-    response = urllib.urlopen('http://www.micb.md/')
-    html = response.read()
-
-    html = html.replace('\r','')
-    html = html.replace('\n', '')
-    html = html.replace(' ', '')
-    html = re.sub(r'^.*currancy-rates', 'currancy-rates', html)
-    html = re.sub(r'</div>.*$', 'currancy-ratesEND', html)
-    html = html.replace('\t', '')
-    html = re.sub(r'</table>.*$', '</table>', html)
-    html = html.replace('</span></td></tr>', '\n')
-    html = re.sub(r'^.*USD', 'USD', html)
-    html = html.replace('</td><td>&mdash;</td><td>&mdash;</td><tdclass="dec"><span>', ' = ')
-    html = html.replace('</td><td>&mdash;</td><td>&mdash;</td><td><span>', ' = ')
-    html = html.replace('<tr><tdclass="currancy">', '')
-    html = html.replace('</tbody></table>', '')
-    return html
 
 db = DBHelper()
 
@@ -70,7 +47,7 @@ db = DBHelper()
 fo = open("token.pw", "r")
 i = fo.readlines();
 
-# Close opend file
+# Close opened file
 fo.close()
 TOKEN = i[0].replace('\"', '')
 TOKEN = TOKEN.replace('\n', '')
@@ -99,12 +76,7 @@ def get_last_chat_id_and_text(updates):
     text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
-"""
-def send_message(text, chat_id):
-    text = urllib.pathname2url(text)
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    get_url(url)
-"""
+
 def send_message(text, chat_id, reply_markup=None):
     text = urllib.pathname2url(text)
     url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
@@ -118,51 +90,6 @@ def get_last_update_id(updates):
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
 
-"""
-def echo_all(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        send_message(text, chat)
-"""
-"""
-def handle_updates(updates):
-    for update in updates["result"]:
-        try:
-            text = update["message"]["text"]
-            chat = update["message"]["chat"]["id"]
-            items = db.get_items()
-            if text in items:
-                db.delete_item(text)
-                items = db.get_items()
-            else:
-                db.add_item(text)
-                items = db.get_items()
-            message = "\n".join(items)
-            send_message(message, chat)
-        except KeyError:
-            pass
-"""
-"""
-def handle_updates(updates):
-    for update in updates["result"]:
-        text = update["message"]["text"]
-        chat = update["message"]["chat"]["id"]
-        items = db.get_items()
-        if text == "/done":
-            keyboard = build_keyboard(items)
-            send_message("Select an item to delete", chat, keyboard)
-        elif text in items:
-            db.delete_item(text)
-            items = db.get_items()
-            keyboard = build_keyboard(items)
-            send_message("Select an item to delete", chat, keyboard)
-        else:
-            db.add_item(text)
-            items = db.get_items()
-            message = "\n".join(items)
-            send_message(message, chat)
-"""
 def handle_updates(updates):
 
     for update in updates["result"]:
@@ -172,36 +99,9 @@ def handle_updates(updates):
         answer = input_check_ip(text)
         send_message(answer, chat)
         if answer == 'Found ip in list':
-            #send_message("Wait a sec...", chat)
             answer = srv_status(text)
             send_message(answer, chat)
-        """
-        if text == "/done":
-            keyboard = build_keyboard(items)
-            send_message("Select an item to delete", chat, keyboard)
-        elif text == '191.96.4.215':
-            send_message("Wait a sec...", chat)
-            response = srv_status(text)
-            send_message(response, chat)
-        elif text == "micb":
-            send_message(getMicb(), chat)
-        elif text == "/start":
-            send_message(
-                "Welcome to your personal To Do list. Send any text to me and I'll store it as an item. Send /done to remove items",
-                chat)
-        elif text.startswith("/"):
-            continue
-        elif text in items:
-            db.delete_item(text, chat)  ##
-            items = db.get_items(chat)  ##
-            keyboard = build_keyboard(items)
-            send_message("Select an item to delete", chat, keyboard)
-        else:
-            db.add_item(text, chat)  ##
-            items = db.get_items(chat)  ##
-            message = "\n".join(items)
-            send_message(message, chat)
-"""
+
 def build_keyboard(items):
     keyboard = [[item] for item in items]
     reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
@@ -213,7 +113,7 @@ def main():
     while True:
         print "Getting updates"
         updates = get_updates(last_update_id)
-	print updates
+        print updates
         if len(updates["result"]) > 0:
             last_update_id = get_last_update_id(updates) + 1
             handle_updates(updates)
